86f9fa7eef7b8fefa008ee3e40310a4d6956afc4
Hiyagg <36637520+Hiyagg@users.noreply.github.com>
2018-06-26 20:06:57 +0800

    Add files via upload
    增加数据库储存历史订单

--- GetOrderList.py
+++ GetOrderList.py
@@ -7,7 +7,12 @@ import hashlib
 import threading
 import logging
 import datetime
+import pymysql
 
+
+"""
+监控支付宝到账通知，获取订单信息，记录并发送回调给服务器
+"""
 logging.basicConfig(level=logging.INFO,
                 format='%(asctime)s %(funcName)s %(levelname)s %(message)s',
                 datefmt='%Y-%m-%d %H:%M:%S',
@@ -17,40 +22,46 @@ logging.basicConfig(level=logging.INFO,
 info = logging.info
 warning = logging.warning
 
+
 class Phone(object):
-    def __init__(self, ip):
-        self.orderlast = [{'billName': '800012345612000212', 'billAmount': '0.20', 'timeInfo': '20180614212100'}]
+
+    mysql = ''  # Mysql连接
+    cur = ''  # 游标
+
+    def __init__(self, ip, acountinfo):
+        self.orderlast = [{'billName': '', 'billAmount': '', 'timeInfo': ''}]
+        self.orderlast[0]['billName'], self.orderlast[0]['billAmount'], self.orderlast[0]['timeInfo'] = self.sql_serch(acountinfo)
         self.ordernew = []
         self.pages = 0
         self.ct = u2.connect(ip)
 
-
-    def findbill(self):#获取界面信息，匹配订单数据，筛选，返回新订单
+    # 获取界面信息，匹配订单数据，筛选，返回新订单
+    def findbill(self):
         time.sleep(0.2)
         info('Try get data')
-
+        # 找到当前屏幕内所有的显示完整订单
         billlist = self.ct.dump_hierarchy()
-        pattern = re.compile('.*(\\d{18,20})[\\s\\S]*?([+]\\d{1,5}\\.\\d\\d)[\\s\\S]*?(\\d\\d:\\d\\d).*')  #匹配订单备注，订单金额，交易时间
-        bills = pattern.findall(billlist) #找到当前屏幕内所有的显示完整订单
+        pattern = re.compile('(\\d{18,20})[\\s\\S]*?([+]\\d{1,5}\\.\\d\\d)[\\s\\S]*?(\\d\\d:\\d\\d)')  #匹配订单备注，订单金额，交易时间
+        bills = pattern.findall(billlist)
         if not bills:
-            warning('No new order , try open app again')#没有读取到订单 重新打开支付宝进入账单界面
+            warning('No new order , try open app again')  # 没有读取到订单 重新打开支付宝进入账单界面
             self.openapp(0)
             return 1
 
         for bill in bills:
-            billname, billamount, timeinfo = bill
+            billname, billamount, timeinfo = bill[0], bill[1], bill[2].replace(':', '')+'00'
             billdata = {'billAmount': billamount[1:], 'billName': billname,
-                        'timeInfo': '%s' % datetime.date.today().strftime('%Y%m%d')+timeinfo.replace(':', '')+'00'} #订单格式
-            if billdata in self.ordernew: #如果已在本次订单列表中，则跳过该笔订单
+                        'timeInfo': '%s' % timeinfo}  # 订单格式
+            if billdata in self.ordernew:  # 如果已在本次订单列表中，则跳过该笔订单
                 continue
-            elif billdata in self.orderlast: #如果已在上次的订单列表中，则结束读取
-                return 0  #返回0，告知完毕
+            elif billdata in self.orderlast:  # 如果已在上次的订单列表中，则结束读取
+                return 0  # 返回0，告知完毕
             else:
-                self.ordernew.append(billdata) #否则添加到新订单列表
+                self.ordernew.append(billdata)  # 否则添加到新订单列表
         info('all bills are new, continue find')
         self.pages += 1
         self.ct.swipe(0.5, 0.85, 0.5, 0.1, 0.5)
-        return 1 #上滑查看更多订单，返回1，继续获取
+        return 1  # 上滑查看更多订单，返回1，继续获取
 
 
     def get(self):
@@ -63,7 +74,7 @@ class Phone(object):
             with open('record.txt', 'w') as f:  # 储存在本地
                 f.write(str(self.ordernew))
             self.orderlast = self.ordernew  # 记录本次的获取订单列表
-        #input('读取获取完成')
+        # input('读取获取完成')
         return self.ordernew
 
     def openapp(self, restart):
@@ -71,7 +82,7 @@ class Phone(object):
         self.ct.app_stop('com.eg.android.AlipayGphone')
         self.ct.app_start('com.eg.android.AlipayGphone')
         self.ct(text='我的').click_exists(5)
-        self.ct(text='账单').click_exists(3)
+        self.ct(text='账单').click_exists(5)
         if restart == 1:
             return 1
 
@@ -80,65 +91,113 @@ class Phone(object):
         info('Start Watching')
         while 1:
             restart = 0
-            time.sleep(0.5)
-            if self.ct.info['currentPackageName'] != 'com.eg.android.AlipayGphone' or self.ct(text='通知').exists:
+            time.sleep(1)
+            if self.ct.info['currentPackageName'] != 'com.eg.android.AlipayGphone':
                 restart = self.openapp(1)
             if self.ct(text='支付宝通知').exists or restart == 1:
                 info('Alipay information，ready to get bills')
-
+                ctime = datetime.datetime.now().strftime('%Y%m%d%H%M%S')  # 当前时间
                 self.ct.swipe(0.5, 0.3, 0.5, 0.6, 0.03)  # 下拉刷新账单
                 self.ordernew = []  # 新订单表清零
 
-                #获取订单信息
+                # 获取订单信息
                 st_time = time.time()
                 order = self.get()
                 use_time = time.time()-st_time
                 info('Delay time%s' % use_time)
-                #print(order)
+                # print(order)
                 if not order:
                     warning('No new order!!!')
                     continue
                 # 发送订单线程
                 info('Create new thread to send data')
-                threading.Thread(target=sendurl, args=(order,)).start()
+                threading.Thread(target=self.send_url, args=(order, ctime)).start()
                 # 返回订单顶部
                 for i in range(self.pages):
                     self.ct.swipe(0.5, 0.1, 0.5, 0.85, 0.3)
                 self.pages = 0
 
-def sendurl(order):#发送回调
-    info('Start send order data')
-    key = '123456'
-    msg = ''
-    for i in order:
-        for x, y in i.items():
-            msg += x + '=' + y + '&'
-        info('Original str: %s' % msg[:-1])
-        msg_key = msg + 'key='+key
-        md5 = hashlib.md5()
-        md5.update(msg_key.encode(encoding='utf-8'))
-        sign = md5.hexdigest()
-        msg_send = msg+'sign='+sign
-        input(msg_send)
-        info('Signed str: %s' % msg_send)
-        for sendtimes in range(2):
-            try:
-                info('Sending')
-                response = requests.post('http://112.74.40.81:11150/cgi-bin/v2.0/api_ali_pay_pqrcode_notify.cgi', data=msg_send)
-                recode = response.text
-                #if recode =! ''
-                info('Reply: %s' % recode)
-                print('Reply: %s' % recode)
-                break
-            except Exception as e:
-                warning('Failed, Error：%s,try again' % e)
-                continue
-    info('Send complete')
+    @classmethod
+    def send_url(cls, order, ctime):  # 发送回调
+        info('Start send order data')
+        key = '123456'
+        for oneorder in order:
+            msg = 'billAmount=%s&billName=%s&timeInfo=%s' % \
+                  (oneorder['billAmount'], oneorder['billName'], ctime[:8] + oneorder['timeInfo'])
+            info('Original str: %s' % msg)
+            msg_key = msg + '&key=' + key
+            md5 = hashlib.md5()
+            md5.update(msg_key.encode(encoding='utf-8'))
+            sign = md5.hexdigest()
+            msg_send = msg + '&sign=' + sign
+            input(msg_send)
+            info('Signed str: %s' % msg_send)
+            order_id = ctime + '%02d' % order.index(oneorder)
+            for sendtimes in range(2):
+                try:
+                    info('Sending')
+                    response = requests.post('http://112.74.40.81:11150/cgi-bin/v2.0/api_ali_pay_pqrcode_notify.cgi',
+                                             data=msg_send)
+                    recode = response.text
+                    print('Reply: %s' % recode)
+                    info('Reply: %s' % recode+' write to db')
+                    if recode == 'success':
+                        cls.sql_insert(ctime, oneorder, order_id, status='1')
+                    else:
+                        cls.sql_insert(ctime, oneorder, order_id, status='2')
+                    break
+                except Exception as e:
+                    if sendtimes == 0:
+                        warning('Failed, Error：%s,try again' % e)
+                    else:
+                        warning('Failed again, Error：%s,write to db' % e)
+                        cls.sql_insert(ctime, oneorder, order_id, status='0')
+                    continue
+            info('Send complete')
+
+    @classmethod
+    def sql_insert(cls, ctime, oneorder, order_id, status):
+        info('connect mysql and insert into table')
+        cls.sql_conn()
+        acountinfo, billname, billamount, timeinfo = oneorder['billName'][:9], oneorder['billName'], \
+                                             oneorder['billAmount'], oneorder['timeInfo']
+        sql = "INSERT INTO `orders`.`new_table` (`acountinfo`, `billname`, `billamount`, `timeinfo`, " \
+              "`ctime`, `status`,`order_id`) VALUES (%s,%s,%s,%s,%s,%s,%s);"\
+              % (acountinfo, billname, billamount, timeinfo, ctime, status, order_id)
+        #input(sql)
+        cls.cur.execute(sql)
+        cls.mysql.commit()
+        cls.sql_close()
+        info('over , close')
+
+    @classmethod
+    def sql_serch(cls, acountinfo):
+        info('connect mysql and search the last order')
+        cls.sql_conn()
+        sql = "SELECT billname,billamount,timeinfo FROM `orders`.`new_table` WHERE acountinfo=%s ORDER BY ctime DESC limit 0,5" % acountinfo
+        #input(sql)
+        cls.cur.execute(sql)
+        order_data = cls.cur.fetchmany(1)[0]
+        #input(order_data)
+        cls.sql_close()
+        info('over , close and return')
+        #return order_data
+        return ('123456002000001001','0.10','200400') #测试
+
+    @classmethod
+    def sql_conn(cls):
+        cls.mysql = pymysql.connect(host='54.176.208.157', port=3306, user='admin', passwd='131415', db='orders')
+        cls.cur = cls.mysql.cursor()
+
+    @classmethod
+    def sql_close(cls):
+        cls.cur.close()
+        cls.mysql.close()
 
 
 if __name__ == '__main__':
-    ip = '192.168.137.47'
-    info('Phone ip: %s' % ip)
-    phone01 = Phone(ip)
+    address = '192.168.43.87'
+    info('Phone ip: %s' % address)
+    phone01 = Phone(address, '123456002')
     phone01.run()
 
--- GetQRcode.py
+++ GetQRcode.py
@@ -3,58 +3,74 @@ import uiautomator2 as u2
 import time
 from config import phonelist
 import threading
-import xlwt
 
-d = [u2.connect(i['ip']) for i in phonelist]
-qrcode = []
+d = [u2.connect(i['ip']) for i in phonelist] #手机列表
 
 
 def getqr(d, user, account, qrmoney, qrnum):
+    print('账户%s正在生成二维码' % account, end='')
+    print(" %s%s%s%s" % (user, account, qrmoney, qrnum))
     d.app_start('com.eg.android.AlipayGphone')
-    #d(text='收钱').click_exists(1)
     d(text='设置金额').click()
-    #d.set_fastinput_ime(True) # 切换成FastInputIME输入法
-    d.send_keys(str(float(qrmoney)/100)) # adb广播输入
-
+    time.sleep(0.2)
+    d.set_fastinput_ime(True)  # 切换成FastInputIME输入法
+    d.send_keys(str(float(qrmoney)/100))  # adb广播输入
+    time.sleep(0.2)
     d(text='添加收款理由').click()
-
-    d.send_keys("%s%s%s%s" % (user, account, qrmoney, qrnum)) # adb广播输入
-
+    time.sleep(0.2)
+    d.send_keys("%s%s%s%s" % (user, account, qrmoney, qrnum))  # adb广播输入
+    time.sleep(0.2)
     d(text='确定').click()
+    time.sleep(0.2)
     d(text='保存图片').click()
-    #time.sleep(1)
+    time.sleep(0.5)
     d(text='清除金额').click()
-
+    time.sleep(0.2)
+    print('账户%s正在扫描二维码' % account)
     d.app_start('mark.qrcode')
+    time.sleep(1)
     d(text='从图库扫描…').click()
+
     if account == '001':
+        time.sleep(0.2)
         d.tap(0.26, 0.32)
     elif account == '002':
+        time.sleep(0.5)
         d.tap(0.282, 0.814)
+        time.sleep(0.2)
         d.tap(0.125, 0.3)
     elif account == '003':
-        d.tap(0.2, 0.3)
-    #time.sleep(0.2)
+        time.sleep(0.5)
+        d.tap(0.26, 0.3)
+    time.sleep(0.5)
+    print('账户%s正在解析二维码' % account)
     url = d(resourceId="mark.qrcode:id/p").get_text()
+    time.sleep(0.5)
     d.press('back')
-    return "%s%s%s%s" % (user, account, qrmoney, qrnum), url
+    return user+account+qrmoney+qrnum+'    '+url
 
 
 def creatbill(i, user, account):
-    qr = []
+    """
+    :param i:手机编码
+    :param user:商户号
+    :param account:支付宝账户编号
+    :return: none
+    """
     print(threading.currentThread())
     t0 = time.time()
-    for qrmoney in [x for x in range(10, 51) if x % 10 == 0]:
+    for qrmoney in [x for x in range(10, 21) if x % 10 == 0]:  # 金额，单位分，七位数
         qrmoney = "%07d" % qrmoney
-        for qrnum in range(1, 3):
+        for qrnum in range(1, 6):  # 某金额二维码编号，两位数
             qrnum = "%02d" % qrnum
-            #input("%s%s%s%s" % (user, account, qrmoney, qrnum))
-            qr.append(getqr(d[i], user, account, qrmoney, qrnum))
-    t = time.time()-t0
-    print(t, qr)
+            qrcode=getqr(d[i], user, account, qrmoney, qrnum)  # 生成一个二维码
+            print('账户%s正在保存二维码' % account)
+            with open('url%s.txt' % (user+account), 'a') as f:
+                f.write(qrcode+'\n')
+    print(time.time() - t0)
 
 
-for i in range(3): #三台手机
+for i in range(1, 3):  # 三台手机
     user, account = phonelist[i]['user'], phonelist[i]['account']
     threading.Thread(target=creatbill, args=(i, user, account)).start()
 print(threading.enumerate())
